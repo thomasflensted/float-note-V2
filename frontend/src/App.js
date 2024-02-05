68ccdb63d42e885ffc3804e86d6a1669225c5e90
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-02-05 16:19:36 +0100

    getting notes from mongodb on load

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react";
+
 function App() {
+
+  const [notes, setNotes] = useState([]);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      const timer = setTimeout(() => setIsLoading(true), 500);
+      try {
+        const res = await fetch('http://localhost:4000/api/notes');
+        if (!res.ok) throw Error("There was an error retrieving the data.")
+        const json = await res.json();
+        setNotes(json);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        clearTimeout(timer);
+        setIsLoading(false);
+      }
+    }
+    fetchNotes();
+  }, [])
+
   return (
-    <h1>Hello, React</h1>
+    <div>
+      {isLoading && <h1>Your Notes Are Loading...</h1>}
+      {error && <h1>{error}</h1>}
+      {notes.map(note =>
+        <li key={note._id}>
+          <h1>{note.heading}</h1>
+        </li>)}
+    </div>
   );
 }
 
