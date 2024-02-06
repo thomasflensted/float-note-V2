b7d41731cb98c2f2724c679be5d7578a237648de
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-02-06 18:35:34 +0100

    updated cors link

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ['https://float-note-backend.vercel.app/'],
+    origin: ['https://float-note-api.vercel.app/'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true
 }));
