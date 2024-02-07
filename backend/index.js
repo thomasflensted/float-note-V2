83d16b0708182abf67b58237fb798eef4f40a37a
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-02-07 11:48:26 +0100

    trying to fix cors issues

--- backend/index.js
+++ backend/index.js
@@ -8,27 +8,12 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-app.use((req, res, next) => {
-    res.setHeader(
-        "Access-Control-Allow-Origin",
-        "https://float-note.onrender.com/"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET,PATCH,POST,DELETE,OPTIONS"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Private-Network", true);
-    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-    res.setHeader("Access-Control-Max-Age", 7200);
-
-    next();
-});
+const cors = require("cors");
+const corsOptions = {
+    origin: "https://float-note.onrender.com/",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
+app.use(cors(corsOptions));
 app.use('/api/notes', routes);
 
 mongoose.connect(process.env.MONGO_URI)
