0876678f74b66c24151967f46d9a9a84158d74ff
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-02-09 14:15:05 +0100

    prevent default on submit

--- frontend/src/components/DialogHeadingForm.jsx
+++ frontend/src/components/DialogHeadingForm.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DialogHeadingForm = ({ text, setHeading }) => {
     return (
-        <form action="">
+        <form action="" onSubmit={(e) => e.preventDefault()}>
             <label className='new-note-label' htmlFor="heading">Heading:</label>
             <input
                 defaultValue={text}
