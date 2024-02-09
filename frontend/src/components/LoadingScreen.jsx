21a49802aba61d3542bfddc590ba68e1e298fb8a
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-02-09 14:24:13 +0100

    created and styled loading animation

--- frontend/src/components/LoadingScreen.jsx
+++ frontend/src/components/LoadingScreen.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { HamburgerMenuIcon, Pencil2Icon } from '@radix-ui/react-icons'
+import { ring } from 'ldrs';
+ring.register()
 
 const LoadingScreen = () => {
     return (
-        <div className='note loading-error'>
-            <h3>Loading notes...</h3>
-        </div>
+        <div className='note loading-note' style={{ width: "300px" }}>
+            <div className="note-top loading-note-top">
+                <h2 className='note-title'>Loading...</h2>
+            </div>
+            <div className="note-text-container loading-note-text-container">
+                <l-ring size="30" stroke="4" speed="2" color="black"></l-ring>
+                <p className='note-text'>Your notes are loading...</p>
+            </div>
+        </div >
     )
 }
 
