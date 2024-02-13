0b19eb407fcc335982f9f2c1210e32cbc4f6ec50
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-02-13 08:45:22 +0100

    created functionality and ui for login and signup

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,13 +1,33 @@
+const UserModel = require('../models/UserModel');
 const User = require('../models/UserModel')
+const jwt = require('jsonwebtoken');
+
+const createToken = (id) => {
+    return jwt.sign({ _id: id }, process.env.SECRET, { expiresIn: '5d' })
+}
 
 // login user
 const loginUser = async (req, res) => {
-    res.json({ mssg: "log in user" })
+    const { email, password } = req.body;
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id)
+        res.status(200).json({ email, token })
+    } catch (err) {
+        res.status(400).json({ error: err.message })
+    }
 }
 
 // signup user 
 const signupUser = async (req, res) => {
-    res.json({ mssg: "sign up user" })
+    const { email, password } = req.body;
+    try {
+        const user = await User.signup(email, password);
+        const token = createToken(user._id)
+        res.status(200).json({ email, token });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 }
 
 module.exports = { loginUser, signupUser }
--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,17 +1,57 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
 })
 
+UserSchema.statics.login = async function (email, password) {
+
+    if (!email || !password) {
+        throw Error("All fields must be filled out.")
+    }
+
+    const user = await this.findOne({ email });
+    if (!user) {
+        throw Error("Wrong email.");
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+        throw Error("Incorrect password.");
+    }
+
+    return user;
+}
+
 // static signup method
-UserSchema.statics.signup = async (username, password) => {
-    const exists = await this.findOne({ username });
+UserSchema.statics.signup = async function (email, password) {
+
+    if (!email || !password) {
+        throw Error("All fields must be filled out.")
+    }
+
+    const exists = await this.findOne({ email });
     if (exists) {
         throw Error("User already exists.");
     }
+
+    if (!validator.isEmail(email)) {
+        throw Error("Invalid email.");
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        throw Error("Password is too weak.");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = await this.create({ email, password: hash });
+    return user;
 }
 
 module.exports = mongoose.model("User", UserSchema);  
