7dc15893030201d5f77430ebf7c3fdaac8edf743
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-02-22 11:04:21 +0100

    updated color select to work with tab select

--- frontend/src/components/ColorSelect.jsx
+++ frontend/src/components/ColorSelect.jsx
@@ -3,19 +3,52 @@ import * as ToggleGroup from '@radix-ui/react-toggle-group';
 import { CrossCircledIcon } from '@radix-ui/react-icons';
 
 const ColorSelect = ({ color, setColor }) => {
+
+    const colors = {
+        white: '#FFFFFF',
+        yellow: '#FAEDCB',
+        green: '#C9E4DE',
+        blue: '#C6DEF1',
+        red: '#FFD2D2',
+        gray: '#ECF0F1',
+    }
+
     return (
         <div>
             <label className='form-label'>Color:</label>
             <ToggleGroup.Root type="single" className="ToggleGroup" defaultValue={color}>
                 <ToggleGroup.Item
-                    onClick={() => setColor('#FFFFFF')} value='#FFFFFF' style={{ backgroundColor: "#FFFFFF" }} className="ToggleGroupItem">
+                    onFocus={() => setColor(colors.white)}
+                    data-state={color === colors.white ? "on" : 'off'}
+                    value={colors.white}
+                    className="ToggleGroupItem white">
                     <CrossCircledIcon className='cross' />
                 </ToggleGroup.Item>
-                <ToggleGroup.Item onClick={() => setColor('#FAEDCB')} value='#FAEDCB' style={{ backgroundColor: "#FAEDCB" }} className="yellow ToggleGroupItem" />
-                <ToggleGroup.Item onClick={() => setColor('#C9E4DE')} value='#C9E4DE' style={{ backgroundColor: "#C9E4DE" }} className="ToggleGroupItem" />
-                <ToggleGroup.Item onClick={() => setColor('#C6DEF1')} value='#C6DEF1' style={{ backgroundColor: "#C6DEF1" }} className="ToggleGroupItem" />
-                <ToggleGroup.Item onClick={() => setColor('#FFD2D2')} value='#FFD2D2' style={{ backgroundColor: "#FFD2D2" }} className="ToggleGroupItem" />
-                <ToggleGroup.Item onClick={() => setColor('#ECF0F1')} value='#ECF0F1' style={{ backgroundColor: "#ECF0F1" }} className="ToggleGroupItem" />
+                <ToggleGroup.Item
+                    onFocus={() => setColor(colors.yellow)}
+                    data-state={color === colors.yellow ? "on" : 'off'}
+                    value={colors.yellow}
+                    className="ToggleGroupItem yellow" />
+                <ToggleGroup.Item
+                    onFocus={() => setColor(colors.green)}
+                    data-state={color === colors.green ? "on" : 'off'}
+                    value={colors.green}
+                    className="ToggleGroupItem green" />
+                <ToggleGroup.Item
+                    onFocus={() => setColor(colors.blue)}
+                    data-state={color === colors.blue ? "on" : 'off'}
+                    value={colors.blue}
+                    className="ToggleGroupItem blue" />
+                <ToggleGroup.Item
+                    onFocus={() => setColor(colors.red)}
+                    data-state={color === colors.red ? "on" : 'off'}
+                    value={colors.red}
+                    className="ToggleGroupItem red" />
+                <ToggleGroup.Item
+                    onFocus={() => setColor(colors.gray)}
+                    data-state={color === colors.gray ? "on" : 'off'}
+                    value={colors.gray}
+                    className="ToggleGroupItem gray" />
             </ToggleGroup.Root>
         </div>
     )
